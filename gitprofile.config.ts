918b2cd896af452b4806bda05b4c6af76e08b7b7
Md. Tanvir Ahmed <tanvirumanuiu@gmail.com>
2024-06-12 13:59:37 +0600

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -34,10 +34,10 @@ const CONFIG = {
       projects: [
         {
           title: 'Project Name',
-           CourseCode: '',
-      CourseTitle: 'Journal Name',
-           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+         description:
+              Course Code: '',
+      Course Title: 'Journal Name',
+           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
           link: 'https://example.com',
