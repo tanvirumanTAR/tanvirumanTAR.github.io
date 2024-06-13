1ca79249a7ebda12abd733e4cf844df90bcabeed
Md. Tanvir Ahmed <tanvirumanuiu@gmail.com>
2024-06-13 11:10:29 +0600

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -270,8 +270,9 @@ const CONFIG = {
     customTheme: {
       primary: '#fc055b',
       secondary: '#219aaf',
-      accent: '#e8d03a',
-      neutral: '#2A2730',
+      accent: '#000000',
+     // accent: '#e8d03a',
+    neutral: '#2A2730',
       'base-100': '#E3E3ED',
       '--rounded-box': '3rem',
       '--rounded-btn': '3rem',
