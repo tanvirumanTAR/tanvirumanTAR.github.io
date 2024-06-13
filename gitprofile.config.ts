12b988dc4747f010f735fb27eab1ce0a1ef7ad83
Md. Tanvir Ahmed <tanvirumanuiu@gmail.com>
2024-06-13 11:50:25 +0600

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -215,17 +215,7 @@ const CONFIG = {
     id: '',
     snippetVersion: 6,
   },
-  <!-- Hotjar Tracking Code for https://tanvirumantar.github.io/ -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5022642,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
+
    themeConfig: {
     defaultTheme: 'pastel',
 
@@ -291,6 +281,17 @@ const CONFIG = {
   },
 
   // Optional Footer. Supports plain text or HTML.
+    <!-- Hotjar Tracking Code for https://tanvirumantar.github.io/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5022642,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 /*  footer: `Made with <a 
       class="text-primary" href="https://github.com/arifszn/gitprofile"
       target="_blank"
