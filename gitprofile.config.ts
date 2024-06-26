672024ba71afaeeb1746e24d3fe40576741446d4
Md. Tanvir Ahmed <tanvirumanuiu@gmail.com>
2024-06-27 02:41:11 +0600

    done

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -217,11 +217,11 @@ const CONFIG = {
   },
 
    themeConfig: {
-    defaultTheme: 'pastel',
+    defaultTheme: 'black',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
