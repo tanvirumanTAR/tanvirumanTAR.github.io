97233d3e665afb27247ebc53fbf91f2d986ab142
Md. Tanvir Ahmed <tanvirumanuiu@gmail.com>
2024-06-27 02:13:00 +0600

    hifh

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -272,7 +272,7 @@ const CONFIG = {
   
       primary: '#e9edf0',
       //primary: '#fc055b',
-      secondary: '#219aaf',
+      secondary: '#16dbcb',
       accent: '#db1616',
      // accent: '#e8d03a',
     neutral: '#000000',
